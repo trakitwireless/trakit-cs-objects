891fae03806d650437171b0ec8777df46e2ba413
Alex Lein <alein@trakitwireless.com>
2024-11-25 15:43:26 -0500

    Organizing session files into Users folder

--- commands/WebSocket/Requests/ReqSubscriptionRemove.cs
+++ commands/WebSocket/Requests/ReqSubscriptionRemove.cs
@@ -1,6 +1,6 @@
 ﻿namespace Trakit.Commands {
 	/// <summary>
-	/// The types of subscriptions available using <see cref="subscribe"/>/<see cref="unsubscribe"/>.
+	/// The types of subscriptions available using <see cref="ReqSubscriptionMerge"/>.
 	/// Each type has a different synchronization messages and objects.
 	/// </summary>
 	public class ReqSubscriptionRemove : ReqSubscriptionMerge { }
