530a10eb410bc16503704681c3898f5551669f33
Alex Lein <alein@trakitwireless.com>
2024-11-26 09:41:34 -0500

    These are not needed

--- commands/Dispatch/DispatchJob/Responses/RespDispatchJobList.cs
+++ commands/Dispatch/DispatchJob/Responses/RespDispatchJobList.cs
@@ -61,29 +61,4 @@ namespace Trakit.Commands {
 		/// <seealso cref="DispatchJob.references"/>
 		public Dictionary<string, string> references { get; set; }
 	}
-
-	/// <summary>
-	/// 
-	/// </summary>
-	public class RespDispatchJobListByUnassigned : RespDispatchJobListByCompany { }
-	/// <summary>
-	/// Gets the list of <see cref="DispatchJob"/>s for the specified <see cref="Company"/> which are not assigned to an <see cref="Asset"/>, only if the <see cref="DispatchJob.labels"/> matches all of the given <see cref="labels"/>.
-	/// </summary>
-	public class RespDispatchJobListByUnassignedAndLabels : RespDispatchJobListByUnassigned, IRespListByLabels {
-		/// <summary>
-		/// A list of <see cref="LabelStyle.code">label codes</see> used to match <see cref="DispatchJob"/>s.
-		/// All labels must match to include a <see cref="DispatchJob"/> in the result.
-		/// </summary>
-		public string[] labels { get; set; }
-	}
-	/// <summary>
-	/// 
-	/// </summary>
-	public class RespDispatchJobListByUnassignedAndRefPairs : RespDispatchJobListByUnassigned, IRespListByReferences {
-		/// <summary>
-		/// Case-insensitive reference pairs used to match jobs.
-		/// </summary>
-		/// <seealso cref="DispatchJob.references"/>
-		public Dictionary<string, string> references { get; set; }
-	}
 }
