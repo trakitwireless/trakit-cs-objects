b1a26a8e836409ac8d67fd61e8d296446c75cb89
Alex Lein <alein@trakitwireless.com>
2024-11-27 18:44:38 -0500

    Added provider config commands

---
+++ commands/Providers/Configs/Requests/ReqProviderConfigBatchDelete.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Trakit.Commands {
+	/// <summary>
+	/// 
+	/// </summary>
+	public class ReqProviderConfigBatchDelete : Request {
+		/// <summary>
+		/// 
+		/// </summary>
+		public List<ParamId> providerConfigs { get; set; }
+	}
+}
---
+++ commands/Providers/Configs/Responses/RespProviderConfigDelete.cs
@@ -0,0 +1,13 @@
+﻿using Trakit.Objects;
+
+namespace Trakit.Commands {
+	/// <summary>
+	/// A container for the <see cref="providerConfig"/>.
+	/// </summary>
+	public class RespProviderConfigDelete : Response {
+		/// <summary>
+		/// Details about deleting/restoring the requested <see cref="ProviderConfig"/>.
+		/// </summary>
+		public RespDeleted providerConfig;
+	}
+}
