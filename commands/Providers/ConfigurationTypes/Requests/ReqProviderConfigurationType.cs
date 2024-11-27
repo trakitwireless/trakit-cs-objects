35c5f5592f5766edb43498db0f29997ea09eb303
Alex Lein <alein@trakitwireless.com>
2024-11-27 18:52:07 -0500

    provconfigtype

---
+++ commands/Providers/ConfigurationTypes/Requests/ReqProviderConfigurationType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Trakit.Objects;
+
+namespace Trakit.Commands {
+	/// <summary>
+	/// A container for the <see cref="providerConfigurationType"/> object.
+	/// </summary>
+	[Obsolete("Use ReqProviderScript instead")]
+	public abstract class ReqProviderConfigurationType : Request, IReqSingle {
+		/// <summary>
+		/// An object to contain the "id" of the <see cref="ProviderConfigurationType"/>.
+		/// </summary>
+		public ParamId providerConfigurationType { get; set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		public string GetKey() => this.providerConfigurationType?.id.ToString() ?? "";
+	}
+}
