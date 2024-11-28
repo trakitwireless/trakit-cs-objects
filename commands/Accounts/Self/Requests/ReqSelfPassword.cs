4a5e10dd5164b9817feb8c69b22f243b28eb0b43
Alex Lein <alein@trakitwireless.com>
2024-11-28 12:25:41 -0500

    Adding remaining Self commands

---
+++ commands/Accounts/Self/Requests/ReqSelfPassword.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Trakit.Objects;
+
+namespace Trakit.Commands {
+	/// <summary>
+	/// Allows a <see cref="User"/> to change their own password.
+	/// </summary>
+	public class ReqSelfPassword : Request {
+		/// <summary>
+		/// Your current password, as verification that you are the proper account owner.
+		/// </summary>
+		[JsonIgnore]
+		public string current;
+		/// <summary>
+		/// Your new password must conform to your company's <see cref="PasswordPolicy">password policy</see>.
+		/// </summary>
+		[JsonIgnore]
+		public string password;
+	}
+}
---
+++ commands/Accounts/Self/Responses/RespSelfContactMerge.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Trakit.Commands {
+	/// <summary>
+	/// 
+	/// </summary>
+	public class RespSelfContactMerge : Response {	}
+}
