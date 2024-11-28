6a54fb788547118505a98b8c561ee2e8d99b2cf3
Alex Lein <alein@trakitwireless.com>
2024-11-27 19:39:43 -0500

    Actually, no billing yet

--- commands/Billing/Rules/Requests/ReqProviderScriptRestore.cs
+++
@@ -1,8 +0,0 @@
-﻿using Trakit.Objects;
-
-namespace Trakit.Commands {
-	/// <summary>
-	/// Restores a deleted <see cref="BillableHostingRule"/>.
-	/// </summary>
-	public class ReqBillableHostingRuleRestore : ReqBillableHostingRule { }
-}
