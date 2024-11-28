2668044074a5ae606f884e47e45bdc1ec65d5c5f
Alex Lein <alein@trakitwireless.com>
2024-11-27 19:48:09 -0500

    Actually, also needs provider control commands

---
+++ commands/Providers/Providers/Requests/ReqProviderControlGet.cs
@@ -0,0 +1,13 @@
+﻿using Trakit.Objects;
+
+namespace Trakit.Commands {
+	/// <summary>
+	/// Gets details of the specified <see cref="ProviderControl"/>.
+	/// </summary>
+	public class ReqProviderControlGet : ReqProvider, IReqIDeletable {
+		/// <summary>
+		/// When true, the command will also return a deleted <see cref="ProviderControl"/> (if it exists).
+		/// </summary>
+		public bool includeDeleted { get; set; }
+	}
+}
---
+++ commands/Providers/Providers/Responses/RespProviderControlGet.cs
@@ -0,0 +1,13 @@
+﻿using Trakit.Objects;
+
+namespace Trakit.Commands {
+	/// <summary>
+	/// A container for the <see cref="providerControl"/>.
+	/// </summary>
+	public class RespProviderControlGet : Response {
+		/// <summary>
+		/// The requested <see cref="ProviderControl"/>.
+		/// </summary>
+		public ProviderControl providerControl;
+	}
+}
