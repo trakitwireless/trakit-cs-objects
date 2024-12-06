ba94ec015a35b3f5c4a6797c1fcb14fec16dd76f
Alex Lein <alein@trakitwireless.com>
2024-12-06 17:46:37 -0500

    We will probably need this for response classes, but should it be a class or an interface?

---
+++ commands/API/Responses/IRespList.cs
@@ -0,0 +1,14 @@
+﻿using Trakit.Objects;
+
+namespace Trakit.Commands {
+	/// <summary>
+	/// 
+	/// </summary>
+	public interface IRespList<TComponent> where TComponent : Component {
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		TComponent[] GetCollection();
+	}
+}
