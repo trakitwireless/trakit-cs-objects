6bafe1c105317365bc8f280b380a2e139a0632cb
Alex Lein <alein@trakitwireless.com>
2024-12-18 14:05:12 -0500

    Removed erroneous [DataMember]s Added StreetAddress.ToString

--- objects/API/Geography/StreetAddress.cs
+++ objects/API/Geography/StreetAddress.cs
@@ -1,6 +1,8 @@
-﻿namespace Trakit.Objects {
+﻿using System.Collections.Generic;
+
+namespace Trakit.Objects {
 	/// <summary>
-	/// A road segment description
+	/// A road segment description.
 	/// </summary>
 	public class StreetAddress {
 		/// <summary>
@@ -39,5 +41,23 @@
 		/// Indicates that there is a toll for the current road segment.
 		/// </summary>
 		public bool isToll;
+
+		/// <summary>
+		/// Returns a text representation of this address.
+		/// </summary>
+		/// <remarks>
+		/// Returned strings cannot be converted back into StreetAddress objects, so don't use this for deserialization.
+		/// </remarks>
+		/// <returns></returns>
+		public override string ToString() {
+			var address = new List<string>();
+			if (!string.IsNullOrWhiteSpace(this.street)) address.Add((!string.IsNullOrWhiteSpace(this.number) ? this.number + " " : "") + this.street);
+			if (!string.IsNullOrWhiteSpace(this.city)) address.Add(this.city);
+			if (!string.IsNullOrWhiteSpace(this.region)) address.Add(this.region);
+			if (!string.IsNullOrWhiteSpace(this.province)) address.Add(this.province);
+			if (!string.IsNullOrWhiteSpace(this.country)) address.Add(this.country);
+			if (!string.IsNullOrWhiteSpace(this.postal)) address.Add(this.postal);
+			return string.Join(", ", address);
+		}
 	}
 }
