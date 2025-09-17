27e2ca06df5a112888efba39892b24226f41c3f8
Alex Lein <alein@trakitwireless.com>
2025-09-17 14:26:50 -0400

    remaining commands also added a couple utility methods to LatLng

--- Objects/API/Geography/LatLng.cs
+++ Objects/API/Geography/LatLng.cs
@@ -1,8 +1,10 @@
-﻿namespace Trakit.Objects {
+﻿using System;
+
+namespace Trakit.Objects {
 	/// <summary>
 	/// A coordinate on the globe
 	/// </summary>
-	public class LatLng {
+	public class LatLng : IEquatable<LatLng> {
 		/// <summary>
 		/// Let's consider the zero-zero coordinates to be invalid.
 		/// </summary>
@@ -21,5 +23,22 @@
 			this.lat = lat;
 			this.lng = lng;
 		}
+		//public override bool Equals(object obj) => obj is LatLng other && this.Equals(other);
+
+		/// <summary>
+		/// True when a <see cref="PlaceType.polygon"/> or <see cref="PlaceType.rectangle"/> shape has enough coordinates given.
+		/// </summary>
+		/// <param name="other"></param>
+		/// <returns></returns>
+		public bool Equals(LatLng other) => other?.isValid() == this.isValid()
+									&& other?.lat == this.lat
+									&& other?.lng == this.lng;
+
+		/// <summary>
+		/// True when a <see cref="PlaceType.radial"/> radius is within the allowed size range.
+		/// </summary>
+		/// <returns></returns>
+		public bool isValid() => !double.IsNaN(this.lat) && !double.IsInfinity(this.lat)
+						&& !double.IsNaN(this.lng) && !double.IsInfinity(this.lng);
 	}
 }
