78d82f7c04f4ba286d5e355d9684142ca25c07fa
Alex Lein <alein@trakitwireless.com>
2026-05-26 15:57:11 -0400

    Renamed classes; we use singular, not plural

--- Objects/Companies/CompanyStyle.cs
+++ Objects/Companies/CompanyStyle.cs
@@ -4,7 +4,7 @@ namespace Trakit.Objects {
 	/// <summary>
 	/// The colours and styles used by this company to tag and label Assets, Places, and other things.
 	/// </summary>
-	public class CompanyStyles : Component, IIdUlong, IAmCompany {
+	public class CompanyStyle : Component, IIdUlong, IAmCompany {
 		/// <summary>
 		/// Unique identifier of the Company.
 		/// </summary>
